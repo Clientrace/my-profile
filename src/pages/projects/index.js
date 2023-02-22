b9f4b8915ceb7979a0262f93baa9085b7de40784
Clarence Penaflor <kimc.penaflor@gmail.com>
2023-02-23 07:53:36 +0800

    add interactive text to projects

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -2,22 +2,37 @@
 import Head from "next/head";
 import Home from "../components/home/home";
 import CMD from "../components/cmd/cmd";
+import { useState } from "react";
 
 
 const Projects = () => {
+  const [cmdText, setCmdText] = useState("> Browse projects");
+  const [cmdSticky, setCmdSticky] = useState(false);
+  const [cmdAccessFlag, setCmdAccessFlag] = useState(false);
+  const setInteractiveText = (text) => {
+    if(cmdAccessFlag){
+      setCmdText(text);
+    }
+  }
+
   return <div>
     <Head>
       <title>Projects</title>
     </Head>
     <Home
       headingText="Projects"
-      subheadingText="<tech stuff & more/>"/>
+      subheadingText="<Tech Stuff & more/>"
+      animationFlag={false}
+      setCmdText={setInteractiveText}
+      setCmdSticky={setCmdSticky}/>
     <CMD 
-      initText={initText}
-      showFlag={commandFlag}
+      showFlag={true}
       setAccessFlag={setCmdAccessFlag}
       cmdText={cmdText}
       sticky={cmdSticky}/>
+    <div>
+
+    </div>
   </div>
 }
 
