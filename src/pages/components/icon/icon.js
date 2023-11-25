0073001f9cc2eae4877fa8db9c0b0001f4c7e708
Kim Clarence Penaflor <kimc.penaflor@gmail.com>
2023-11-25 10:30:26 +0800

    refactor

--- src/pages/components/icon/icon.js
+++ src/pages/components/icon/icon.js
@@ -1,28 +1,27 @@
+import styles from "./icon.module.scss";
+import { motion } from "framer-motion";
 
-import styles from './icon.module.scss';
-import { motion } from 'framer-motion';
-
-
-const Icon = ({src, url, delay, onHoverStart}) => {
+const Icon = ({ src, url, delay, onHoverStart, size = "35px" }) => {
   return (
     <>
-      {url?<motion.a
-        whileHover={{opacity: 0.3}}
-        href={url} target="_blank">
-        <motion.img
-          alt={url}
-          onHoverStart={onHoverStart}
-          initial={{scale: 1.3, opacity: 0}}
-          animate={{scale: 1, opacity: 1}}
-          whileHover={{scale: 1.2}}
-          transition={{delay: delay}}
-          src={src} className={styles['image']}/>
-      </motion.a>:<motion.img src={src} alt="tech image" className={styles['image']}/>}
+      {url ? (
+        <motion.a whileHover={{ opacity: 0.3 }} href={url} target="_blank">
+          <motion.img
+            alt={url}
+            onHoverStart={onHoverStart}
+            initial={{ scale: 1.3, opacity: 0 }}
+            animate={{ scale: 1, opacity: 1 }}
+            whileHover={{ scale: 1.2 }}
+            transition={{ delay: delay }}
+            src={src}
+            style={{ width: size }}
+          />
+        </motion.a>
+      ) : (
+        <motion.img src={src} alt="tech image" className={{ width: size }} />
+      )}
     </>
-  )
-}
+  );
+};
 
 export default Icon;
-
-
-
