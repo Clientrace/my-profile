7c12297852f8afa7b4f6d0df801c6099ef09a735
Kim Clarence Penaflor <kimc.penaflor@gmail.com>
2023-11-25 10:00:30 +0800

    implement refactored home-v2

--- src/pages/components/homev2/index.js
+++ src/pages/components/homev2/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { motion } from "framer-motion";
 
+import Icon from "../../components/icon/icon";
 import Name from "../animations/name/name";
 import styles from "./styles.module.scss";
 
@@ -11,6 +12,7 @@ const Home = ({
   setCmdSticky,
   animationFlag,
 }) => {
+  animationFlag = true;
   const titleAnimation = animationFlag && {
     initial: { opacity: 0 },
     animate: { opacity: 1 },
@@ -37,6 +39,32 @@ const Home = ({
         <motion.h4 {...titleAnimation}>
           <div className="text-sm sm:text-lg">{subheadingText}</div>
         </motion.h4>
+      </div>
+      <div className="flex gap-x-2 align-center text-center justify-center">
+        <Icon
+          onHoverStart={() => setCmdText("> Jump to my facebook profile")}
+          src="/assets/facebook.svg"
+          url="https://web.facebook.com/kcpenaflor11"
+          delay={animationFlag ? 5.2 : 0.1}
+        />
+        <Icon
+          onHoverStart={() => setCmdText("> Jump to my github page")}
+          src="/assets/github.svg"
+          url="https://github.com/Clientrace"
+          delay={animationFlag ? 5.4 : 0.2}
+        />
+        <Icon
+          onHoverStart={() => setCmdText("> View my linkedin page")}
+          src="/assets/linkedin.svg"
+          url="https://www.linkedin.com/in/kcpenaflor"
+          delay={animationFlag ? 5.6 : 0.3}
+        />
+        <Icon
+          onHoverStart={() => setCmdText("> Visit my instagram")}
+          src="/assets/instagram.svg"
+          url="https://www.instagram.com/kcpenaflor/"
+          delay={animationFlag ? 5.8 : 0.4}
+        />
       </div>
     </div>
   );
