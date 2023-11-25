69c9fd03ab783c5589f9af2b0c423bc5d0fe4bd5
Kim Clarence Penaflor <kimc.penaflor@gmail.com>
2023-11-25 09:54:25 +0800

    add initial logic

--- src/pages/components/homev2/index.js
+++ src/pages/components/homev2/index.js
@@ -1,4 +1,8 @@
 import React from "react";
+import { motion } from "framer-motion";
+
+import Name from "../animations/name/name";
+import styles from "./styles.module.scss";
 
 const Home = ({
   headingText,
@@ -7,7 +11,35 @@ const Home = ({
   setCmdSticky,
   animationFlag,
 }) => {
-  return <div className="w-full" style={{ height: "30vh" }}></div>;
+  const titleAnimation = animationFlag && {
+    initial: { opacity: 0 },
+    animate: { opacity: 1 },
+    transition: { delay: 5 },
+  };
+
+  return (
+    <div
+      className={`${styles["Main"]} flex w-full text-white justify-center align-center text-center flex-col`}
+      style={{ height: "30vh" }}
+    >
+      <div className="tracking-wider">
+        <motion.h1
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          onHoverStart={() => setCmdText("> Yep, that's my name")}
+        >
+          <div className="text-2xl sm:text-4xl">
+            <Name text={headingText} initSpeed={300} />
+          </div>
+        </motion.h1>
+      </div>
+      <div className={styles["mainSubtitle"]}>
+        <motion.h4 {...titleAnimation}>
+          <div className="text-sm sm:text-lg">{subheadingText}</div>
+        </motion.h4>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
