c4c544d332f9efc9f71ad41be87d715539ab875e
Kim Clarence Penaflor <kimc.penaflor@gmail.com>
2023-11-25 09:34:42 +0800

    add home v2 using tailwind

---
+++ src/pages/components/homev2/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = ({
+  headingText,
+  subheadingText,
+  setCmdText,
+  setCmdSticky,
+  animationFlag,
+}) => {
+  return <div className="w-full" style={{ height: "30vh" }}></div>;
+};
+
+export default Home;
