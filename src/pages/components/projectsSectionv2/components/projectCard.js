ad8f5f6a1fceade3610bf11fa5c0263996288e24
Kim Clarence Penaflor <kimc.penaflor@gmail.com>
2023-11-26 21:23:04 +0800

    add project card

---
+++ src/pages/components/projectsSectionv2/components/projectCard.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+export const ProjectCard = ({ name, subtitle, tech = [], linkoutUrl }) => {
+  return <motion.div></motion.div>;
+};
