1b5745c6518864ea4156f9ea6517701c1b2c5e21
Kim Clarence Penaflor <kimc.penaflor@gmail.com>
2023-11-26 21:23:28 +0800

    update mobile layout

--- src/pages/components/heading/heading.js
+++ src/pages/components/heading/heading.js
@@ -1,11 +1,26 @@
 import Icon from "../../components/icon/icon";
 
-const Heading = ({ children, iconUrl = null }) => {
+const Heading = ({
+  children,
+  description = "'",
+  subtitle = "",
+  iconUrl = null,
+}) => {
   return (
-    <h2 className="flex align-middle gap-x-2">
-      <Icon src={iconUrl ? iconUrl : "/assets/block.svg"} />
-      {children}
-    </h2>
+    <div className="w-full">
+      <div className="w-full">
+        <div className="sm:flex align-middle gap-x-2">
+          <div className="sm:mt-2 mt-2 sm:-ml-6">
+            <Icon src={iconUrl ? iconUrl : "/assets/block.svg"} small />
+          </div>
+          <diV className="text-base sm:text-3xl tracking-wider">
+            {children}
+            <div className="sm:text-sm text-xs mt-0 sm:mt-1">{subtitle}</div>
+          </diV>
+        </div>
+        <div className="mt-1 text-sm sm:text-base">{description}</div>
+      </div>
+    </div>
   );
 };
 
