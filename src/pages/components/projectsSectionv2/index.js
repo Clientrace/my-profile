d09ceea8b3f2b603396987932eb4caa3ce798cc5
Kim Clarence Penaflor <kimc.penaflor@gmail.com>
2023-12-09 06:25:54 +0800

    add section wrapper

--- src/pages/components/projectsSectionv2/index.js
+++ src/pages/components/projectsSectionv2/index.js
@@ -1,68 +1,66 @@
 import React, { useState } from "react";
 
-import { motion } from "framer-motion";
-
 import Name from "../animations/name/name";
 import Heading from "../heading/heading";
 import Divider from "../divider/divider";
+import SectionWrapper from "../sectionWrapper";
+
 import { ProjectCard } from "./components/projectCard";
 
 const ProjectSection = ({ hoverAction, animationFlag }) => {
   const [loaded, setLoaded] = useState(false);
   return (
-    <motion.div className="h-auto" onHoverStart={() => hoverAction()}>
-      <div className="w-full">
-        <Heading
-          subtitle="Personal Projects/Pet Projects"
-          description="I have worked on a lot of pet projects, some are publicly viewable on my github account. Here are top 3 of my projects:"
-          iconUrl="/assets/folder.svg"
-          desc
+    <SectionWrapper hoverAction={hoverAction}>
+      <Heading
+        subtitle="Personal Projects/Pet Projects"
+        description="I have worked on a lot of pet projects, some are publicly viewable on my github account. Here are top 3 of my projects:"
+        iconUrl="/assets/folder.svg"
+        desc
+      >
+        {loaded || !animationFlag ? (
+          "Projects"
+        ) : (
+          <Name
+            text="Projects"
+            initSpeed={200}
+            teardown={() => setLoaded(true)}
+          />
+        )}
+      </Heading>
+      <Divider />
+      <div className="flex flex-wrap w-full gap-2 scroll-smooth overflow-auto">
+        <ProjectCard
+          name="Friday Chatbot"
+          imageUrl="/assets/friday.png"
+          platform="Facebook"
+          year="2018"
+          tech={["python"]}
         >
-          {loaded || !animationFlag ? (
-            "Projects"
-          ) : (
-            <Name
-              text="Projects"
-              initSpeed={200}
-              teardown={() => setLoaded(true)}
-            />
-          )}
-        </Heading>
-        <Divider />
-        <div className="flex flex-wrap w-full gap-2 scroll-smooth overflow-auto">
-          <ProjectCard
-            name="Friday Chatbot"
-            imageUrl="/assets/friday.png"
-            platform="Facebook"
-            year="2018"
-            tech={["python"]}
-          >
-            A facebook messenger chatbot for answer simple search queries.
-            Heavily relies on messenger API.
-          </ProjectCard>
-          <ProjectCard
-            name="Pymicro"
-            imageUrl="/assets/pymicro.png"
-            platform="AWS Lambda"
-            year="2022"
-            tech={["python"]}
-          >
-            Serverless AWS Framework for creating API. Has it's own CLI for code
-            scaffolding.
-          </ProjectCard>
-          <ProjectCard
-            name="Inxscape"
-            imageUrl="/assets/inxscape.png"
-            platform="Web Application"
-            year="2020"
-            tech={["python"]}
-          >
-            Headless Web Content Management Tool that utilizes Markdown to
-            generate content.
-          </ProjectCard>
-        </div>
+          A facebook messenger chatbot for answer simple search queries. Heavily
+          relies on messenger API.
+        </ProjectCard>
+        <ProjectCard
+          name="Pymicro"
+          imageUrl="/assets/pymicro.png"
+          platform="AWS Lambda"
+          year="2022"
+          tech={["python"]}
+        >
+          Serverless AWS Framework for creating API. Has it's own CLI for code
+          scaffolding.
+        </ProjectCard>
+        <ProjectCard
+          name="Inxscape"
+          imageUrl="/assets/inxscape.png"
+          platform="Web Application"
+          year="2020"
+          tech={["python"]}
+        >
+          Headless Web Content Management Tool that utilizes Markdown to
+          generate content.
+        </ProjectCard>
       </div>
-    </motion.div>
+    </SectionWrapper>
   );
 };
 
