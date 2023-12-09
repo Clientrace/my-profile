ff95edb120166fc93eca59b1c8edcf1da95844aa
Kim Clarence Penaflor <kimc.penaflor@gmail.com>
2023-12-09 18:38:16 +0800

    update wrapper spacing

--- src/pages/components/sectionWrapper/index.js
+++ src/pages/components/sectionWrapper/index.js
@@ -4,9 +4,10 @@ import Heading from "../heading/heading";
 const SectionWrapper = ({ hoverAction, children }) => {
   return (
     <motion.div
-      className="h-auto"
+      className="h-auto my-4 sm:my-8"
       onHoverStart={() => hoverAction()}
-      style={{ opacity: 0.8 }}
+      whileHover={{ opacity: 1 }}
+      style={{ opacity: 0.95 }}
     >
       <div className="w-full">{children}</div>
     </motion.div>
