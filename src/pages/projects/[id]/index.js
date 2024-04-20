1e0b6de7abd241707254cb56011210ca7c215992
Clarence Penaflor <kimc.penaflor@gmail.com>
2024-04-21 00:56:34 +0800

    remove serverless

--- src/pages/projects/[id]/index.js
+++ src/pages/projects/[id]/index.js
@@ -1,13 +1,30 @@
 import Head from "next/head";
+import { useEffect } from "react";
+import Banner from "../../components/banner";
 
 
-const ProjectDescription = () => {
-  return <div>
-    
+
+const ProjectDescription = (props) => {
+  const { id } = props;
+
+
+  useEffect(()=>{
+
+  },[])
+
+
+  return <div className="w-full">
+    <Banner/>
   </div>
 }
 
+
+ProjectDescription.getInitialProps = async (ctx) => {
+  const d = {
+    id: ctx.query.id
+  }
+  return d;
+}
+
 export default ProjectDescription;
-
-
 
