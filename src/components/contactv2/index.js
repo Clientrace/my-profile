027ef70552ef3b28a3c7bee8e9280f39313a7e4c
Clarence Penaflor <kimc.penaflor@gmail.com>
2024-04-22 01:59:55 +0800

    add profile to contact

--- src/components/contactv2/index.js
+++ src/components/contactv2/index.js
@@ -13,33 +13,50 @@ const Contact = ({ setCmdText, hoverAction, animationFlag }) => {
         Contact
       </Heading>
       <Divider />
-      <div className="w-full">
-        <div>Email: kimc.penalfor@gmail.com</div>
-        <div>Mobile: +639260797073</div>
-        <div>
-          Socials:
-          <div className="mt-1">
-            <div className="flex gap-x-2">
-              <Icon
-                onHoverStart={() => setCmdText("> Jump to my facebook profile")}
-                src="/assets/facebook_dark.svg"
-                url="https://web.facebook.com/kcpenaflor11"
-              />
-              <Icon
-                onHoverStart={() => setCmdText("> Jump to my github page")}
-                src="/assets/github_dark.svg"
-                url="https://github.com/Clientrace"
-              />
-              <Icon
-                onHoverStart={() => setCmdText("> View my linkedin page")}
-                src="/assets/linkedin_dark.svg"
-                url="https://www.linkedin.com/in/kcpenaflor"
-              />
-              <Icon
-                onHoverStart={() => setCmdText("> Visit my instagram")}
-                src="/assets/instagram_dark.svg"
-                url="https://www.instagram.com/kcpenaflor/"
-              />
+      <div className="flex w-max gap-x-3.5">
+        <div className="w-full">
+          <img src="/assets/profile.jpeg" className="w-24 h-24" />
+        </div>
+        <div className="w-full">
+          <div>
+            <div className="text-sm">
+              Email:
+            </div>
+            kimc.penalfor@gmail.com
+          </div>
+          <div className="mt-2">
+            <div className="text-sm">
+              Mobile:
+            </div>
+            +639260797073
+          </div>
+          <div className="mt-2">
+            <div className="text-sm">
+              Socials:
+            </div>
+            <div className="mt-1">
+              <div className="flex gap-x-2">
+                <Icon
+                  onHoverStart={() => setCmdText("> Jump to my facebook profile")}
+                  src="/assets/facebook_dark.svg"
+                  url="https://web.facebook.com/kcpenaflor11"
+                />
+                <Icon
+                  onHoverStart={() => setCmdText("> Jump to my github page")}
+                  src="/assets/github_dark.svg"
+                  url="https://github.com/Clientrace"
+                />
+                <Icon
+                  onHoverStart={() => setCmdText("> View my linkedin page")}
+                  src="/assets/linkedin_dark.svg"
+                  url="https://www.linkedin.com/in/kcpenaflor"
+                />
+                <Icon
+                  onHoverStart={() => setCmdText("> Visit my instagram")}
+                  src="/assets/instagram_dark.svg"
+                  url="https://www.instagram.com/kcpenaflor/"
+                />
+              </div>
             </div>
           </div>
         </div>
