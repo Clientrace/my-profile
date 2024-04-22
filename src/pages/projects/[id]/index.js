136a2dcd5556a3b9467782633364237dea7cae80
Clarence Penaflor <kimc.penaflor@gmail.com>
2024-04-23 06:08:27 +0800

    implement markdown loading

--- src/pages/projects/[id]/index.js
+++ src/pages/projects/[id]/index.js
@@ -1,20 +1,49 @@
 import Head from "next/head";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Banner from "../../../components/banner";
+import Markdown from "react-markdown";
 
 
+const s3HostUrl = "https://clarence-webprofile-articles.s3.ap-southeast-1.amazonaws.com/projects";
 
 const ProjectDescription = (props) => {
   const { id } = props;
+  const [project, setProject] = useState({});
+  const [mdText, setMdText] = useState();
 
+  useEffect(() => {
+    getArticle();
+  }, [])
 
-  useEffect(()=>{
+  const getArticle = async () => {
+    fetch(`${s3HostUrl}/${id}/info.json`).then((data) => data.json().then(json => {
+      setProject(json);
+      fetch(`${s3HostUrl}/${id}/article.md`).then((data) => data.text().then(body => {
+        setMdText(body)
+      }));
+    }));
 
-  },[])
+  }
 
 
   return <div className="w-full">
-    <Banner/>
+    <Head>
+      <title>Project</title>
+    </Head>
+    <Banner />
+
+    <div className="flex justify-center px-6 sm:px-40 md:px-40 py-4 sm:py-10 bg-white mt-20">
+      <div className="max-w-2xl">
+        <div className="w-full">
+          Friday Chatbot Project
+        </div>
+        <div className="w-full mt-4">
+          <Markdown>
+            {mdText}
+          </Markdown>
+        </div>
+      </div>
+    </div>
   </div>
 }
 
@@ -23,6 +52,8 @@ ProjectDescription.getInitialProps = async (ctx) => {
   const d = {
     id: ctx.query.id
   }
+
+
   return d;
 }
 
