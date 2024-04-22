3101287ef9f84ada02ef391ffab940a1d448c9fd
Clarence Penaflor <kimc.penaflor@gmail.com>
2024-04-23 06:20:46 +0800

    add loading page

--- src/pages/projects/[id]/index.js
+++ src/pages/projects/[id]/index.js
@@ -2,6 +2,8 @@ import Head from "next/head";
 import { useEffect, useState } from "react";
 import Banner from "../../../components/banner";
 import Markdown from "react-markdown";
+import Icon from "../../../components/icon/icon";
+import Divider from "../../../components/divider/divider";
 
 
 const s3HostUrl = "https://clarence-webprofile-articles.s3.ap-southeast-1.amazonaws.com/projects";
@@ -25,6 +27,12 @@ const ProjectDescription = (props) => {
 
   }
 
+  if (!mdText || !project) {
+    return <div className="flex w-screen h-screen text-white justify-center align-middle bg-black">
+      L O A D I N G
+    </div>
+  }
+
 
   return <div className="w-full">
     <Head>
@@ -34,9 +42,20 @@ const ProjectDescription = (props) => {
 
     <div className="flex justify-center px-6 sm:px-40 md:px-40 py-4 sm:py-10 bg-white mt-20">
       <div className="max-w-2xl">
-        <div className="w-full">
-          Friday Chatbot Project
+        <div className="flex w-full gap-x-4">
+          <div>
+            <Icon src="/assets/folder.svg" />
+          </div>
+          <div>
+            <div>
+              {project['title']}
+            </div>
+            <div className="text-xs text-gray-600">
+              {project['platform']}
+            </div>
+          </div>
         </div>
+        <Divider />
         <div className="w-full mt-4">
           <Markdown>
             {mdText}
