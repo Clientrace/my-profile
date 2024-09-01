fce5088cc76d1f62190918ebec99e2071bb91b95
Clarence Penaflor <kimc.penaflor@gmail.com>
2024-09-01 13:36:24 +0800

    remove clsx

--- src/components/genericLoader/index.js
+++ src/components/genericLoader/index.js
@@ -1,6 +1,6 @@
 import styles from "./styles.module.scss";
-import clsx from "clsx";
 
 export const GenericLoader = ({ color = "gray" }) => {
-  return <div className={clsx(styles["loader"], `text-${color}-100`)} />;
+  const loaderClassnames = `${styles["loader"]} text-${color}-100`;
+  return <div className={loaderClassnames} />;
 };
