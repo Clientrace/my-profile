8a8cbfb3d11a5b70420e256503163824f5f44155
Clarence Penaflor <kimc.penaflor@gmail.com>
2024-09-02 11:14:05 +0800

    update footer

--- src/components/contactv2/index.js
+++ src/components/contactv2/index.js
@@ -15,29 +15,25 @@ const Contact = ({ setCmdText, hoverAction, animationFlag }) => {
       <Divider />
       <div className="flex w-max gap-x-3.5">
         <div className="w-full">
-          <img src="/assets/profile.jpeg" className="w-24 h-24" />
+          <img src="/assets/display-profile.png" className="w-24 h-24" />
         </div>
         <div className="w-full">
           <div>
-            <div className="text-sm">
-              Email:
-            </div>
+            <div className="text-sm">Email:</div>
             kimc.penalfor@gmail.com
           </div>
           <div className="mt-2">
-            <div className="text-sm">
-              Mobile:
-            </div>
+            <div className="text-sm">Mobile:</div>
             +639260797073
           </div>
           <div className="mt-2">
-            <div className="text-sm">
-              Socials:
-            </div>
+            <div className="text-sm">Socials:</div>
             <div className="mt-1">
               <div className="flex gap-x-2">
                 <Icon
-                  onHoverStart={() => setCmdText("> Jump to my facebook profile")}
+                  onHoverStart={() =>
+                    setCmdText("> Jump to my facebook profile")
+                  }
                   src="/assets/facebook_dark.svg"
                   url="https://web.facebook.com/kcpenaflor11"
                 />
--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,44 +1,43 @@
-
 import Icon from "../icon/icon";
 
 const Footer = () => {
-  return <div className="w-full flex justify-center p-20 bg-white">
-    <div className="flex w-fit px-12 gap-x-4">
-      <img src="/assets/profile.jpeg" className="w-24 h-24" />
-      <div className="w-full">
-        <div>
-          Kim Clarence Peñaflor
-        </div>
-        <div className="text-gray-600 text-sm">Full Stack Software Engineer</div>
-        <div className="mt-1">
-          <div className="flex gap-x-2">
-            <Icon
-              onHoverStart={() => setCmdText("> Jump to my facebook profile")}
-              src="/assets/facebook_dark.svg"
-              url="https://web.facebook.com/kcpenaflor11"
-            />
-            <Icon
-              onHoverStart={() => setCmdText("> Jump to my github page")}
-              src="/assets/github_dark.svg"
-              url="https://github.com/Clientrace"
-            />
-            <Icon
-              onHoverStart={() => setCmdText("> View my linkedin page")}
-              src="/assets/linkedin_dark.svg"
-              url="https://www.linkedin.com/in/kcpenaflor"
-            />
-            <Icon
-              onHoverStart={() => setCmdText("> Visit my instagram")}
-              src="/assets/instagram_dark.svg"
-              url="https://www.instagram.com/kcpenaflor/"
-            />
+  return (
+    <div className="w-full flex justify-center pb-20 bg-white">
+      <div className="flex w-fit px-12 gap-x-4">
+        <img src="/assets/display-profile.png" className="w-24 h-24" />
+        <div className="w-full">
+          <div>Kim Clarence Peñaflor</div>
+          <div className="text-gray-600 text-sm">
+            Full Stack Software Engineer
+          </div>
+          <div className="mt-1">
+            <div className="flex gap-x-2">
+              <Icon
+                onHoverStart={() => setCmdText("> Jump to my facebook profile")}
+                src="/assets/facebook_dark.svg"
+                url="https://web.facebook.com/kcpenaflor11"
+              />
+              <Icon
+                onHoverStart={() => setCmdText("> Jump to my github page")}
+                src="/assets/github_dark.svg"
+                url="https://github.com/Clientrace"
+              />
+              <Icon
+                onHoverStart={() => setCmdText("> View my linkedin page")}
+                src="/assets/linkedin_dark.svg"
+                url="https://www.linkedin.com/in/kcpenaflor"
+              />
+              <Icon
+                onHoverStart={() => setCmdText("> Visit my instagram")}
+                src="/assets/instagram_dark.svg"
+                url="https://www.instagram.com/kcpenaflor/"
+              />
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-}
-
+  );
+};
 
 export default Footer;
-
