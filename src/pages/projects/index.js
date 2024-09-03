8dc161d70eb5d1bf333c9b613435a9f7ec1692c7
Clarence Penaflor <kimc.penaflor@gmail.com>
2024-09-03 19:56:33 +0800

    add projects footer

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -12,7 +12,7 @@ import Subheading from "../../components/subheading/subheading";
 import Navigator from "../../components/navigator";
 
 import Divider from "../../components/divider/divider";
-import { PageLoadWrapper } from "../../components/pageLoadWrapper";
+import Footer from "../../components/footer";
 import { ProjectCard } from "../../components/projectsSectionv2/components/projectCard";
 
 const Projects = () => {
@@ -90,6 +90,7 @@ const Projects = () => {
           </div>
         </div>
       </div>
+      <Footer />
     </div>
   );
 };
